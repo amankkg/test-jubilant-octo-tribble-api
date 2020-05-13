5603f559462487010f869af05fac64b9a1f9c26b
Aman Kubanychbek <amankubanychbek@gmail.com>
2020-05-13 18:51:35 +0600

    set cors header

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const app = express()
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", '*')
+  next()
+})
+
 app.get('/api', (req, res) => {
   res.send('Hello World!')
 })
