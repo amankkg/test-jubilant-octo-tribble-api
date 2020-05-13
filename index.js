f36b214016324d656103e24bafa87c88be405c9d
Aman Kubanychbek <amankubanychbek@gmail.com>
2020-05-13 18:54:23 +0600

    add cors method rule

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express()
 app.use(express.json())
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", '*')
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,POST')
   next()
 })
 
@@ -20,4 +21,6 @@ app.post('/api/echo', (req, res) => {
   res.json(req.body.message)
 })
 
-app.listen(process.env.PORT, () => console.log('Listening port ' + process.env.PORT))
+app.listen(process.env.PORT, () =>
+  console.log('Listening port ' + process.env.PORT)
+)
